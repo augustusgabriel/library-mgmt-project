996c5c50787a8ac808eac8dbd0cd938bf1d606f8
Gabriel Augusto <Avgvstvs_G@protonmail.com>
2026-04-22 22:19:28 -0300

    feat(api): add response interceptor with token refresh and retry logic

--- frontend/library-mgmt/src/App.tsx
+++ frontend/library-mgmt/src/App.tsx
@@ -3,9 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { setupInterceptors } from './services/interceptors'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  setupInterceptors();
 
   return (
     <>
---
+++ frontend/library-mgmt/src/services/interceptors.ts
@@ -0,0 +1,32 @@
+import api from './api';
+import type { AxiosRequestConfig } from 'axios';
+import { refreshToken, logout } from './auth';
+
+// evita que TS reclame de tipagem
+interface CustomAxiosRequestConfig extends AxiosRequestConfig {
+    _retry?: boolean;
+}
+
+export function setupInterceptors(){
+    api.interceptors.response.use(
+        response => response, // simplesmente retorna a response sem alterar nada
+        async error => { // caso API responda com erro
+            const originalRequest = error.config as CustomAxiosRequestConfig; // pega request original
+
+            // Se erro foi 401 e é o 1° retry
+            if (error.response?.status === 401 && !originalRequest._retry) {
+                originalRequest._retry = true; // Modifica valor do retry pra evitar loop
+
+                try {
+                    await refreshToken(); // tenta renovar token
+                    return api(originalRequest); // após renovar, refaz request original
+                } catch {
+                    logout(); // se não renovou, logout
+                }
+            }
+
+            // Propaga erro, ou seja, se refresh falhou ou não foi 401, joga erro pro front
+            return Promise.reject(error);
+        }
+    )
+}
